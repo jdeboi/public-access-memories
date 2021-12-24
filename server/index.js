2268275c00fd845b448a630e26ab08c22508b2b2
Jenna deBoisblanc <jdeboi@gmail.com>
2021-12-24 15:22:59 -0800

    serve client index.html

--- server/index.js
+++ server/index.js
@@ -24,6 +24,16 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+})
+
+
 server.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
