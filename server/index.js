67de0d2f1fba593a573c0624d19c33cd6b0fa853
Jenna deBoisblanc <jdeboi@gmail.com>
2021-12-24 15:30:50 -0800

    almost there?

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const http = require("http");
+const path = require('path');
 const server = http.createServer(app);
 
 // sockets
