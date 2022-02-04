9f85812164bc8a23c219432f42c276b024ff358b
Jenna deBoisblanc <jdeboi@gmail.com>
2022-02-04 13:56:28 -0800

    ivans

--- client/src/views/rooms/R_05/Creature.js
+++ client/src/views/rooms/R_05/Creature.js
@@ -7,6 +7,7 @@ class Creature {
         this.x = x;
         this.y = y;
         this.imgs = [];
+        this.timer = this.getRandomT(p5);
 
         const ivansURL = "https://jdeboi-public.s3.us-east-2.amazonaws.com/public_access_memories/home_body/Ivans/"
 
@@ -30,11 +31,26 @@ class Creature {
         p5.translate(-imgW/2, -imgH/2);
         p5.text(this.txt, this.x, this.y+20+imgH, imgW, 1000);
         p5.pop();
+
+        this.move(p5);
+    }
+
+    move(p5) {
+        if (p5.millis()- this.timer > 0) {
+            this.timer = p5.millis() +  this.getRandomT(p5);
+            this.x = p5.random(-1400, 1400);
+            this.y = p5.random(-1400, 1400);
+        }
+
     }
 
     getMouseDist(p5, pagePos) {
         const d = p5.dist(this.x, this.y, p5.mouseX+pagePos.x, p5.mouseY+pagePos.y);
         return d;
+    }
+
+    getRandomT(p5) {
+        return p5.random(4000,12000);
     }
 }
 
