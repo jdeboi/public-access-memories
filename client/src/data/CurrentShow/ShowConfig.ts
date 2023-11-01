563834510b7029bb501d206a745f41512313ecc0
jdeboi <jdeboi@gmail.com>
2023-11-01 16:57:35 -0500

    going live

--- client/src/data/CurrentShow/ShowConfig.ts
+++ client/src/data/CurrentShow/ShowConfig.ts
@@ -3,7 +3,7 @@ const development = (process.env.NODE_ENV === "development");
 // underConstruction: !development,
 
 export const ShowConfig = {
-    isClosed: true,
+    isClosed: false,
     isOpenCallOpen: false,
     underConstruction: false,
     isMenuOn: false,
