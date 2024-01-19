0029381e09903de2b0fc6c6e2734628633b5685b
jdeboi <jdeboi@gmail.com>
2024-01-19 15:59:32 -0600

    redirecting vesper

--- client/src/views/rooms/R_09/Room.tsx
+++ client/src/views/rooms/R_09/Room.tsx
@@ -1,14 +1,33 @@
+import { useEffect } from 'react';
 import './Vesper.css';
+
+// store
+import { useSelector } from 'react-redux';
+import { selectWindow } from '../../../store/store';
+
 
 const Room = () => {
 
+    const windowUI = useSelector(selectWindow);
+
+    useEffect(() => {
+        if (windowUI.compositionStarted) {
+            window.open("https://nonplace.site/shared-folder", '_blank');
+        }
+
+    }, [windowUI.compositionStarted])
+
+
     return (
         <div className="Room Vesper Sketch">
-            <iframe
+                <div className="LoadingPage" style={{ position: "absolute", bottom: 80, right: 10 }}>
+                    <p>see open tab</p>
+                </div>
+            {/* <iframe
                 src={"https://nonplace.site/shared-folder"}
                 width={"100%"}
                 height={"100%"}
-            />
+            /> */}
         </div>
     )
 };
