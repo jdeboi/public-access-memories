f85020be236e3ea2aef743217d210c3e01790e76
jdeboi <jdeboi@gmail.com>
2024-08-05 23:47:12 -0500

    long popup

--- client/src/views/Gallery/Gallery4HomeOffices/components/Popups/Popups.tsx
+++ client/src/views/Gallery/Gallery4HomeOffices/components/Popups/Popups.tsx
@@ -6,7 +6,7 @@ import Frame from "../../../../../components/Frame/Frame";
 
 export default function Popups() {
   const NUM_ADS = 14;
-  const POPUP_LASTS = 8000;
+  const POPUP_LASTS = 18000;
   const POPUP_MIN_INTERVAL = 20000;
   const POPUP_MAX_INTERVAL = 30000;
 
