e3f626e98aa199dd3016c91ebbb6ae563afadc96
Omayeli Arenyeka <aomayeli@gmail.com>
2025-05-23 22:25:32 -0400

    Add iframe

--- client/src/views/rooms/R_00/Room.tsx
+++ client/src/views/rooms/R_00/Room.tsx
@@ -1,60 +1,19 @@
 import React from "react";
 
-import "./Yeli.css";
-
+// This component isn't being used in place of the Gallery Sketch
 const Room = () => {
   return (
     <div
-      className="Room Yeli Sketch"
+      className="Room Sketch"
       style={{ position: "relative", width: "100%", height: "100%" }}
     >
-      <p>
-        {" "}
-        at every point in the trail people are walking ahead of me deeper inside
-        and walking in the opposite direction towards the city, towards the
-        exit, towards the familiar{" "}
-      </p>
-      <p> I cannot always see them all at once </p>
-      <p> The people ahead of me</p>
-      <p> And </p>
-      <p> The people who are leaving </p>
-      <p> Some take other routes out </p>
-      <p> See things I do not see </p>
-      <p> Decide now is a good time as ever to start the journey home </p>
-      <p> Maybe they know something I don’t </p>
-      <p> When I see the people in front of me</p>
-      <p> Trudging forward </p>
-      <p> I think</p>
-      <p> Yes I am going the right way </p>
-      <p> Yes I am on track </p>
-      <p> Yes there is something worth seeing ahead </p>
-      <p> Yes this view is worthy of the climb </p>
-      <p> When I see people speed past in the opposite direction I think </p>
-      <p> Oh it’s getting late </p>
-      <p> They’re turning back and I’m not even halfway there </p>
-      <p> There’s nothing really to see</p>
-      <p> At the end </p>
-      <p> I stopped a man on his way back</p>
-      <p> Asked him </p>
-      <p> Should I keep going? Is it worth it? Or is it more of the same?</p>
-      <p> He couldn’t tell me </p>
-      <p> I kept going when I could </p>
-      <p> And turned back when I couldn’t </p>
-      <p>
-        {" "}
-        Tried to be okay missing whatever people were still walking towards
-      </p>
-      <p> I saw the sunset </p>
-      <p> Over the city </p>
-      <p> It was beautiful </p>
-      <p> And It was enough </p>
-      <p> Today I look at the plants on my walk </p>
-      <p> Plants that know exactly what to do </p>
-      <p> Which way to lean to catch the sunlight</p>
-      <p> Know what the sunlight will do for them </p>
-
-      <p> How do they know which way to bend? </p>
-      <p> Do they follow the heat? The light?</p>
+      <iframe
+        src="https://heartfelt-tanuki-2798f5.netlify.app/"
+        width="100%"
+        height="calc(100% + 50px)"
+        style={{ marginTop: "-50px", border: "none", background: "white" }}
+        title="under construction"
+      ></iframe>
     </div>
   );
 };
