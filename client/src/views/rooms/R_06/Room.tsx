459ab43a16df196e5caec2e8bbca96c3f9a0cf53
jdeboi <jdeboi@gmail.com>
2025-07-09 14:54:43 -0500

    vimeo for eddie

--- client/src/views/rooms/R_06/Room.tsx
+++ client/src/views/rooms/R_06/Room.tsx
@@ -16,7 +16,7 @@ const Room = () => {
         title="Eddie's Studio"
       ></iframe> */}
       {/* <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;"> */}
-      <iframe
+      {/* <iframe
         src="https://www.youtube.com/embed/aPwxdgwGwbI?si=tPirpo3y3esqI5CJ"
         // style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
         // frameborder="0"
@@ -25,6 +25,16 @@ const Room = () => {
         height="100%"
         width="100%"
         title="360° YouTube Video"
+      ></iframe> */}
+      <iframe
+        src="https://player.vimeo.com/video/1100093366?badge=0&autopause=0&player_id=0&app_id=58479"
+        // style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
+        // frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture; gyroscope; accelerometer; vr"
+        allowFullScreen
+        height="100%"
+        width="100%"
+        title="360° Vimeo Video"
       ></iframe>
       {/* </div> */}
     </div>
